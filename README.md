111ff3557a6a6b0d6e851336624734405abf9415
Trevor Carlson <trevorac99@gmail.com>
2021-02-24 20:48:35 -0600

    refactor and update readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # left_click_bot
 A simple program that makes a left click every 6 seconds, though the duration between clicks can be easily adjusted in the code. Once the program is running, press the Delete key to start the clicking. In order to stop the clicking, click the right mouse button and wait until the beginning of the next click cycle. To start the clicking, just press the Delete key. To shutdown the program, press the Backspace key.
 
+## Why does this exist?
+Simple. I wanted to do something in a game while afk. The think I needed to do involved periodic left clicking. I had an idea, and now this exists.
+
 ## Compatibility
 This has a dependency on the `inputbot` crate and should work on any platform supported by `inputbot`. At the time of writing this, the crates.io page for `inputbot` says that it supports Windows and Linux.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use inputbot::{KeybdKey::*, MouseButton, *};
 use std::{
     io::{stdout, Write},
-    sync::{Arc, Mutex},
+    sync::Mutex,
     thread::sleep,
     time::Duration,
 };
@@ -10,58 +10,42 @@ const SECONDS_BETWEEN_CLICKS: u64 = 6;
 const SECOND: Duration = Duration::from_secs(1);
 
 fn main() {
-    println!("Starting up left_click_bot.");
-    println!("Press Delete to start the click loop.");
-    println!("Right click to toggle continue_clicking.");
-    println!("Press Backspace to shut down.");
+    println!(include_str!("startup_message.txt"));
 
-    let state = Arc::new(Mutex::new(State::new()));
+    let state = Mutex::new(State::new());
 
-    {
-        let state = state.clone();
-
-        DeleteKey.bind(move || {
-            {
-                let mut state = state.lock().unwrap();
-                if state.already_clicking {
-                    println!("Can't start a click loop since one is already going.");
-                    return;
-                } else {
-                    state.already_clicking = true;
-                    println!("Starting a click loop.");
-                }
-            }
-            loop {
-                MouseButton::LeftButton.click();
-
-                for _ in 0..SECONDS_BETWEEN_CLICKS {
-                    {
-                        let mut state = state.lock().unwrap();
-                        if !state.continue_clicking {
-                            state.already_clicking = false;
-                            state.continue_clicking = true;
-                            println!("Stopped click loop.");
-                            return;
-                        }
-                    }
-
-                    sleep(SECOND);
-                }
-            }
-        });
-    }
-
-    {
-        let state = state.clone();
-        MouseButton::RightButton.bind(move || {
+    DeleteKey.bind(move || {
+        {
             let mut state = state.lock().unwrap();
             if state.already_clicking {
                 state.continue_clicking = false;
                 print!("Stopping click loop...");
                 stdout().flush().unwrap();
+                return;
+            } else {
+                state.already_clicking = true;
+                println!("Starting a click loop.");
             }
-        });
-    }
+        }
+        loop {
+            MouseButton::LeftButton.press();
+            MouseButton::LeftButton.release();
+
+            for _ in 0..SECONDS_BETWEEN_CLICKS {
+                {
+                    let mut state = state.lock().unwrap();
+                    if !state.continue_clicking {
+                        state.already_clicking = false;
+                        state.continue_clicking = true;
+                        println!("Stopped click loop.");
+                        return;
+                    }
+                }
+
+                sleep(SECOND);
+            }
+        }
+    });
 
     BackspaceKey.bind(|| {
         println!("Shutting down");
@@ -82,16 +66,5 @@ impl State {
             continue_clicking: true,
             already_clicking: false,
         }
-    }
-}
-
-trait Clickable {
-    fn click(self);
-}
-
-impl Clickable for MouseButton {
-    fn click(self) {
-        self.clone().press();
-        self.release();
     }
 }
